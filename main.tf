a4f52a5d4a62a929311a6006165d18111f7d71af
jeonyoungwoo <26229501+jeonyoungwoo@users.noreply.github.com>
2022-03-04 18:48:39 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,14 +4,14 @@ resource "intersight_ippool_pool" "ippool_pool1" {
   assignment_order = "sequential"
    ip_v4_config {
      moid        = var.ippool_ip_v4_config
-     object_type = "ippool.IpV4Config"
+     object_type = "ip_pool"
      gateway     = "10.100.23.1"
      netmask     = "255.255.255.0"
      primary_dns = "8.8.8.8"
    }
     ip_v4_blocks {
     from  = "10.100.23.200"     //시작ip
-    object_type = "ippool.IpV4Config"
+    object_type = "ip_pool"
     size  = "10"                //ip할당갯수
     to = "10.100.23.209"        //끝 ip
   }
@@ -21,11 +21,13 @@ resource "intersight_ippool_pool" "ippool_pool1" {
    }
 }
 
- variable "ippool_ip_v4_config" {
-   type = string
-   description = "IP_Pool"
- }
+
  variable "organization" {
    type = string
    description = "CIP"
  }
+variable "ip_pool" {
+  type        = string
+  description = "Name of IP Pool used by IMC Access Policy"
+  default     = "IP_Pool"   //변경
+}
